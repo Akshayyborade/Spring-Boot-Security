2c051dc3908b4d47adc1354d0fcae0d4bc5a4ce9
Akshay borade <122223892+Akshayyborade@users.noreply.github.com>
2023-10-29 23:59:26 +0530

    security

--- src/main/java/com/springSecurity/config/SecurityConfig.java
+++ src/main/java/com/springSecurity/config/SecurityConfig.java
@@ -1,24 +1,32 @@
 package com.springSecurity.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+
 public class SecurityConfig {
     // creating Basic Authorization using http security
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-            )
-            .httpBasic(withDefaults());
+        http     .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authz) -> authz
+                                .requestMatchers("/public/userlogin")
+                                .permitAll()
+                                .anyRequest().authenticated()
+                )
+
+                .formLogin(login -> login
+                        .loginPage("/public/userlogin")
+                        .loginProcessingUrl("/dologin")
+                        .defaultSuccessUrl("/users"));
+                        
         return http.build();
     }
     //in memory Authorization
@@ -30,6 +38,12 @@ public class SecurityConfig {
     			.roles("user")
     			.build();
     	return new InMemoryUserDetailsManager(userDetails);
+    }
+    //page customization
+  
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder(10);
     }
 
 }
